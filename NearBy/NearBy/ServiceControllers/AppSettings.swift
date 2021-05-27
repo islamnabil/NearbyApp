6e841415798e6b5a7b5fa3a34b479f22b8d381eb
Islam Nabil <islamelgaafary@gmail.com>
2021-05-27 17:23:51 +0200

    Add documentation to AppSettings file

--- NearBy/NearBy/ServiceControllers/AppSettings.swift
+++ NearBy/NearBy/ServiceControllers/AppSettings.swift
@@ -8,15 +8,36 @@
 import UIKit
 
 class AppSettings {
+    
+    //MARK:-  Singleton
     private init(){}
     
+    /// Access the singleton instance
     static var shared = AppSettings()
     
+    
+    // MARK:- Properties
+    private enum status: String {
+        case realTime = "Realtime"
+        case singleUpdate = "Single Update"
+     }
+    
+    // MARK:- Public Functions
+    
+    /// Get current realtime mode status & set the button's title with the status
+    ///
+    /// - Parameters:
+    ///   - statusInfoFor: `UIBarButtonItem` to display current `Mode` (Default value for button = nil)
     func isRealTimeMode(statusInfoFor button:UIBarButtonItem? = nil) -> Bool {
         button?.title = modeName()
         return UserDefaults.standard.bool(forKey: "realTimeMode")
     }
     
+    /// Update current `Mode` to be `Realtime` OR `Single Update`, set the button's title with the status, update `LocationController` with new mode.
+    ///
+    /// - Parameters:
+    ///   - realtime: realtime mode to be `true` OR `false`
+    ///   - statusInfoFor: `UIBarButtonItem` to display current `Mode` (Default value for button = nil)
     func updateMode(realtime:Bool, statusInfoFor button:UIBarButtonItem? = nil) {
         UserDefaults.standard.setValue(realtime, forKey:"realTimeMode")
         button?.title = modeName()
@@ -24,13 +45,10 @@ class AppSettings {
     }
     
     
+    // MARK:- Private Functions
     private func modeName() -> String {
         return isRealTimeMode() ? status.realTime.rawValue : status.singleUpdate.rawValue 
     }
     
-    enum status: String {
-        case realTime = "Realtime"
-        case singleUpdate = "Single Update"
-     }
     
 }
